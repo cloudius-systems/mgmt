2b97b5708cd7afc5bbe41db5d1e3c2fb2d76f4ac
narkisr <narkisr@gmail.com>
2013-09-03 20:28:44 +0300

    adding reloader and adjusting upload to support upload js code

--- web/lib/mgmt.rb
+++ web/lib/mgmt.rb
@@ -1,1 +1,2 @@
+require 'mgmt/env'
 require 'mgmt/app'
--- web/lib/mgmt/app.rb
+++ web/lib/mgmt/app.rb
@@ -1,12 +1,15 @@
 require 'rubygems'
 require 'sinatra'
 require 'mustache/sinatra'
+require "sinatra/reloader" if Mgmt::Env.development?
 require 'java'
+require 'json'
 
 
 module Mgmt
   class App < Sinatra::Base
     register Mustache::Sinatra
+    register Sinatra::Reloader if Mgmt::Env.development?
     require 'views/layout'
 
     set :mustache, {
@@ -23,16 +26,22 @@ module Mgmt
 	mustache :index
     end
 
-    get "/upload" do
+    get '/upload' do
 	mustache :upload
     end       
 
     # Handle POST-request (Receive and save the uploaded file)
-    post "/upload" do  
-	File.open('/uploads/' + params['myfile'][:filename], "w") do |f|
-	  f.write(params['myfile'][:tempfile].read) 
+    post '/upload' do  
+	h = params['files']
+	File.open('/uploads/' + h[:filename], 'w') do |dest|
+	  dest.write(h[:tempfile].read) 
 	end
-	return "The file was successfully uploaded!"
+	res = {"files"=> 
+              [ { "name" => h[:filename] , "size"=> 902604, "url"=> "http://example.org/files/picture1.jpg",
+			"thumbnailUrl"=> "http://fortawesome.github.io/Font-Awesome/icon/laptop",
+			"deleteUrl"=> "http:\/\/example.org\/files\/picture1.jpg", "deleteType"=> "DELETE" }]}
+
+	return res.to_json
     end
   end
 end
