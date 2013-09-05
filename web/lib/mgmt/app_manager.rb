0fbc0a855f7d2a15cc8b12f2ee9537ce025f66bc
narkisr <narkisr@gmail.com>
2013-09-06 00:12:30 +0300

    implemeting launch and scan

--- web/lib/mgmt/app_manager.rb
+++ web/lib/mgmt/app_manager.rb
@@ -1,38 +1,73 @@
 require 'json'
 require 'pathname'
 require 'singleton'
-java_import 'RunJava'
+
+java_import 'org.xeustechnologies.jcl.JarClassLoader'
+java_import 'org.xeustechnologies.jcl.JclObjectFactory'
+
 
 STATE_TO_ACT = {:running => :stop, :stopped => :start}
 
 module Mgmt
   class AppManager
-     include Singleton
+    include Singleton
 
+    # apps is the global running apps tracking store, it contains all running apps:
+    # {"439493befe8870fe5f52eb821ce9cf76" => {:state => :running, :instance=> object}}
     def initialize
-      @state = {}
+	@apps = {}
+	scan
     end
 
     def list
-      apps = Pathname.new(Mgmt::Env.conf(:apps)).children.select { |c| c.directory? }
-      apps.map do |p| 
-	  s = @state[p.basename]
-	  s ||= :stopped
-	  json = JSON.parse(Pathname::glob("#{p}/*.json").first.read)
-	  json.merge({:state => s ,:action => STATE_TO_ACT[s], :id => p.basename})
-	end
+	scan
+	@apps.map {|id, v| {:id => id}.merge(v) }
     end
 
     def start(id)
-      path = "#{Mgmt::Env.conf(:apps)}/#{id}"
-      file = Pathname::glob("#{path}/*.json").first.read
-      args = JSON.parse(file,{:symbolize_names => true})[:args]
-    	 RunJava.new(path).parseArgs(args.split(' '))
-      @state[id] = :running 
+	file = Pathname::glob("#{path(id)}/*.json").first.read
+	conf = JSON.parse(file,{:symbolize_names => true})
+	launch(id,conf)
+	@apps[id][:state] = :running 
+      :stop 
     end
 
     def stop(id)
-	@state[id] = :stopped
+	@apps[id][:instance].stop
+      @apps[id][:instance] = nil
+	@apps[id][:state] = :stopped
+      :start 
+    end
+
+    private
+    def path(id)
+	"#{Mgmt::Env.conf(:apps)}/#{id}"
+    end
+
+    # scans apps folder and updates @apps
+    def scan
+	app_dirs = Pathname.new(Mgmt::Env.conf(:apps)).children.select { |c| c.directory?}
+	app_dirs.each do |p|
+	  json = JSON.parse(Pathname::glob("#{p}/*.json").first.read)
+	  unless @apps[p.basename.to_s] 
+	    @apps[p.basename.to_s] = json.merge({:state => :stopped ,:action => :start})
+	  end
+	end 
+    end
+
+    def launch(id,conf)
+	 Thread.new do
+	  jcl = JarClassLoader.new
+	  jcl.add(path(id))
+	  main = jcl.loadClass(conf[:main])
+	  if main.java_instance_methods.map {|m| m.name.to_sym}.include? :run 
+	    instance = JclObjectFactory.getInstance.create(jcl,conf[:main])
+	    @apps[id][:instance] = instance
+	    instance.run()
+	  else # using its main method
+	    main.java_method(:main).invoke_static(nil)
+	  end
+	end
     end
   end
 end
