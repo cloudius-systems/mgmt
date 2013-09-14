68267f848bdd0c8085c343333e4ec06ad5a28e85
Or Cohen <orc@fewbytes.com>
2013-09-14 19:56:46 +0300

    Fixed java command to pass all args to RunJava

--- crash/src/main/resources/crash/commands/cloudius/java.groovy
+++ crash/src/main/resources/crash/commands/cloudius/java.groovy
@@ -1,15 +1,14 @@
 package crash.commands.cloudius
 
-import org.crsh.cli.Argument
 import org.crsh.cli.Command
-import org.crsh.cli.Required
 import org.crsh.cli.Usage
 
 class java {
   @Usage("Java application launcher")
   @Command
-  Object main(
-    @Required @Argument List<String> argv) {
-    RunJava.main(argv.toArray())
+  Object main() {
+    // "unmatched" contains all of the params not matched by arguments/options
+    // Passing it as-is to RunJava
+    RunJava.main(unmatched.split(' '))
   }
 }
