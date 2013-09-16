72186b1426548cdafb4083cb61bdcfc86cee9bf4
Or Cohen <orc@fewbytes.com>
2013-09-16 13:08:58 +0300

    Added path completion to ls

--- crash/src/main/resources/crash/commands/cloudius/ls.groovy
+++ crash/src/main/resources/crash/commands/cloudius/ls.groovy
@@ -1,14 +1,31 @@
 package crash.commands.cloudius
 
+import com.cloudius.cli.command.OSvCommand
+import com.cloudius.cli.completers.PathCompleter
+import org.crsh.cli.Argument
 import org.crsh.cli.Command
 import org.crsh.cli.Usage
+import org.crsh.cli.descriptor.ParameterDescriptor
+import org.crsh.cli.spi.Completer
+import org.crsh.cli.spi.Completion
 
-class ls {
+// TODO: Implement ls for multiple paths
+
+class ls extends OSvCommand implements Completer {
   @Usage("list directory contents")
   @Command
-  Object main() {
-    out.print(getCurrentPath().listFiles().collect({
-      it.getName()
-    }).join('\t'))
+  String main(
+    @Argument(completer = ls.class) String path
+  ) {
+    pathToList = new File(getCurrentPath(), path == null ? '' : path)
+    files = pathToList.isDirectory() ? pathToList.listFiles() : [pathToList]
+    itemsToColumns(files.collect({ it.isDirectory() ? it.getName() + "/" : it.getName() }), context.width)
+  }
+
+  @Override
+  Completion complete(ParameterDescriptor parameter, String prefix) throws Exception {
+    new PathCompleter(context.getSession().get("getCurrentPath")()).complete(
+      parameter, prefix
+    )
   }
 }
