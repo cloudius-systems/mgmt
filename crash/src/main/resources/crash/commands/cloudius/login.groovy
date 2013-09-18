287b014cf709e9c46692c59f87b70ebf056114b5
Or Cohen <orc@fewbytes.com>
2013-09-18 22:12:55 +0300

    Migrated run command from previous CLI
    Also implemented another little helper to resolve paths passed in from commands
    
    Signed-off-by: Or Cohen <orc@fewbytes.com>

--- crash/src/main/resources/crash/commands/cloudius/login.groovy
+++ crash/src/main/resources/crash/commands/cloudius/login.groovy
@@ -1,13 +1,13 @@
 package crash.commands.cloudius
 
 import com.cloudius.cli.tests.TestRunner
-import org.crsh.cli.completers.FileCompleter
 
 // Path related
 currentPath = new File('/')
 
 getCurrentPath = { currentPath }
 setCurrentPath = { currentPath = it }
+getResolvedPath = { it.startsWith('/') ? new File(it) : new File(getCurrentPath(), it) }
 
 // Test runner
 testRunner = new TestRunner()
---
+++ crash/src/main/resources/crash/commands/cloudius/run.groovy
@@ -0,0 +1,43 @@
+package crash.commands.cloudius
+
+import com.cloudius.cli.command.OSvCommand
+import com.cloudius.cli.completers.PathCompleter
+import com.cloudius.util.ELFLoader
+import org.crsh.cli.Argument
+import org.crsh.cli.Command
+import org.crsh.cli.Required
+import org.crsh.cli.descriptor.ParameterDescriptor
+import org.crsh.cli.spi.Completer
+import org.crsh.cli.spi.Completion
+
+class run extends OSvCommand implements Completer {
+  @Command
+  void main(@Argument(completer = run.class) @Required String path) {
+    def file = getResolvedPath(path)
+
+    if (!file.exists()) {
+      throw new ScriptException("file not found")
+    } else if (!file.isFile()) {
+      throw new ScriptException("not a file")
+    }
+
+    daemonizeIfNeeded(unmatched, { argv ->
+      def command = [file.getCanonicalPath()]
+      command.addAll(argv.split(" "))
+
+      def elfLoader = new ELFLoader()
+      if (elfLoader.run(command as String[])) {
+        out.println("run: finished with exitcode ${elfLoader.lastExitCode()}")
+      } else {
+        out.println("run: failed to execute ${path}")
+      }
+    })
+  }
+
+  @Override
+  Completion complete(ParameterDescriptor parameter, String prefix) throws Exception {
+    new PathCompleter(context.getSession().get("getCurrentPath")()).complete(
+      parameter, prefix
+    )
+  }
+}
