bc25e6d0cbcff9a2669cb078ae5b5c6390f30b32
Or Cohen <orc@fewbytes.com>
2013-11-07 16:19:24 +0200

    shell: Add file completion to md5sum
    Signed-off-by: Or Cohen <orc@fewbytes.com>

--- crash/src/main/resources/crash/commands/cloudius/md5sum.groovy
+++ crash/src/main/resources/crash/commands/cloudius/md5sum.groovy
@@ -1,18 +1,29 @@
 package crash.commands.cloudius
 
+import com.cloudius.cli.completers.PathCompleter
 import com.cloudius.util.MD5
 import org.crsh.cli.Argument
 import org.crsh.cli.Command
 import org.crsh.cli.Required
+import org.crsh.cli.descriptor.ParameterDescriptor
+import org.crsh.cli.spi.Completer
+import org.crsh.cli.spi.Completion
 
-class md5sum {
+class md5sum implements Completer {
   @Command
   Object main(
-    @Required @Argument String path) {
+    @Required @Argument(completer = md5sum.class) String path) {
     file = getResolvedPath(path)
     if (!file.exists()) {
         throw new ScriptException("no such file or directory")
     }
     MD5.md5(file.getPath())
+  }
+
+  @Override
+  Completion complete(ParameterDescriptor parameterDescriptor, String s) throws Exception {
+    new PathCompleter(context.getSession().get("getCurrentPath")()).complete(
+      parameterDescriptor, s
+    )
   }
 }
