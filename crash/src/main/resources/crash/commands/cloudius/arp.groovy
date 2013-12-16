fa1bb1ac8093c1b6c619260142e65c5a5d65ddcf
Or Cohen <orc@fewbytes.com>
2013-12-16 12:18:01 +0200

    Shell: Added descriptions to arp command
    Also, set 'add' as a subcommand.
    
    Fixes #3
    
    Signed-off-by: Or Cohen <orc@fewbytes.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- crash/src/main/resources/crash/commands/cloudius/arp.groovy
+++ crash/src/main/resources/crash/commands/cloudius/arp.groovy
@@ -4,13 +4,16 @@ import com.cloudius.cli.util.Networking
 import org.crsh.cli.Argument
 import org.crsh.cli.Command
 import org.crsh.cli.Required
+import org.crsh.cli.Usage
 
-class arp {
+@Usage("manipulate the system ARP cache")
+public class arp {
+  @Usage("add ARP cache entry")
   @Command
-  Object main(
-    @Argument @Required ifname,
-    @Argument @Required macaddr,
-    @Argument @Required ip) {
+  public void add(
+    @Argument @Required @Usage("Interface name") String ifname,
+    @Argument @Required @Usage("MAC address") String macaddr,
+    @Argument @Required @Usage("IP address") String ip) {
     Networking.arp_add(ifname, macaddr, ip)
   }
 }
