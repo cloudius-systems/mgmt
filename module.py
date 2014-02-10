3f4c420757f228272078232247352b70f5a55e6b
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-02-10 11:56:55 +0100

    Set logging properties on app level
    This way logging configuration of mgmt will not interfere with other
    apps.
    
    Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- module.py
+++ module.py
@@ -10,23 +10,21 @@ usr_files.add('${OSV_BASE}/mgmt/swagger-ui/dist').to('/usr/mgmt/swagger-ui/dist'
 
 _cloudius_jar = '/java/cloudius.jar'
 _web_jar = '/usr/mgmt/web-1.0.0.jar'
-_logging_opts = '-Djava.util.logging.config.file=/usr/mgmt/config/logging.properties'
+_logging_opts = ['-Djava.util.logging.config.file=/usr/mgmt/config/logging.properties']
 
 shell = run_java(
-        jvm_args=[_logging_opts],
         classpath=[
             '/usr/mgmt/lib/bcprov-jdk15on-147.jar',
             '/usr/mgmt/lib/bcpkix-jdk15on-147.jar',
             _cloudius_jar,
             _web_jar,
         ],
-        args=[
+        args=_logging_opts + [
             '-jar', '/usr/mgmt/crash-1.0.0.jar'
         ])
 
 web = run_java(
-        jvm_args=[_logging_opts],
-        args=['-jar', _web_jar, 'app', 'prod'])
+        args=_logging_opts + ['-jar', _web_jar, 'app', 'prod'])
 
 httpserver = api.run('/usr/mgmt/httpserver.so')
 
