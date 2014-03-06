662630e26f28f672de0e1e2fb26c0e3ebaf967ae
Takuya ASADA <syuu@cloudius-systems.com>
2014-03-06 22:18:12 +0900

    add rm command
    Signed-off-by: Takuya ASADA <syuu@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

---
+++ crash/src/main/resources/crash/commands/cloudius/rm.groovy
@@ -0,0 +1,37 @@
+package crash.commands.cloudius
+
+import com.cloudius.cli.completers.PathCompleter
+import org.crsh.cli.Argument
+import org.crsh.cli.Required
+import org.crsh.cli.Usage
+import org.crsh.cli.Command
+import org.crsh.cli.descriptor.ParameterDescriptor
+import org.crsh.cli.spi.Completer
+import org.crsh.cli.spi.Completion
+
+// TODO: Allow multiple files to be specified
+
+class rm implements Completer {
+  @Usage("delete file or directory")
+  @Command
+  void main(
+  @Required @Argument(completer = rm.class) String path
+  ) {
+    file = getResolvedPath(path)
+
+    if (!file.exists()) {
+      throw new ScriptException("no such file or directory")
+    }
+    if (!file.delete()) {
+      throw new ScriptException("cannot delete file")
+    }
+    out.println("file deleted")
+  }
+
+  @Override
+  Completion complete(ParameterDescriptor parameterDescriptor, String s) throws Exception {
+    new PathCompleter(context.getSession().get("getCurrentPath")()).complete(
+      parameterDescriptor, s
+    )
+  }
+}
