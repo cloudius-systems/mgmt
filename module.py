34f5a6c3a2e7db46a8d8c6a8fde07dcb983337e6
Or Cohen <orc@fewbytes.com>
2014-03-10 18:33:40 +0200

    Shell: Stop building an uberjar for CRaSH
    This patch removes the complicated build we had for CRaSH which created an
    uberjar. Dependencies are copied using the build module's FileMap.
    
    The crash jar is also placed in its own classpath, before the other library
    jars because our class overrides one of CRaSH's internal classes which is
    loaded with ServiceLoader.
    
    Signed-off-by: Or Cohen <orc@fewbytes.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- module.py
+++ module.py
@@ -7,16 +7,20 @@ usr_files = FileMap()
 usr_files.add('${OSV_BASE}/mgmt/httpserver/httpserver.so').to('/usr/mgmt/httpserver.so')
 usr_files.add('${OSV_BASE}/mgmt/api').to('/usr/mgmt/api')
 usr_files.add('${OSV_BASE}/mgmt/swagger-ui/dist').to('/usr/mgmt/swagger-ui/dist')
+usr_files.add('${OSV_BASE}/mgmt/crash/build/dependencies').to('/usr/mgmt/lib')
 
+_crash_jar = '/usr/mgmt/crash-1.0.0.jar'
 _cloudius_jar = '/java/cloudius.jar'
 _logging_opts = ['-Djava.util.logging.config.file=/usr/mgmt/config/logging.properties']
 
 shell = run_java(
         classpath=[
-            _cloudius_jar
+            _cloudius_jar,
+            _crash_jar,
+            '/usr/mgmt/lib/*'
         ],
         args=_logging_opts + [
-            '-jar', '/usr/mgmt/crash-1.0.0.jar'
+            '-jar', _crash_jar
         ])
 
 httpserver = api.run('/usr/mgmt/httpserver.so')
