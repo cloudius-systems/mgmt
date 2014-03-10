717c17618e409aaeac2ce221e1e8992b354d204d
Or Cohen <orc@fewbytes.com>
2014-03-10 18:33:44 +0200

    Shell: Add help and man commands
    Changing the command path to our own hides these commands. This brings them
    back.
    
    Signed-off-by: Or Cohen <orc@fewbytes.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

---
+++ crash/src/main/resources/crash/commands/cloudius/help.java
@@ -0,0 +1,52 @@
+package crash.commands.cloudius;
+
+import org.crsh.cli.Command;
+import org.crsh.cli.Usage;
+import org.crsh.command.BaseCommand;
+import org.crsh.command.InvocationContext;
+import org.crsh.shell.impl.command.CRaSH;
+import org.crsh.text.Color;
+import org.crsh.text.Decoration;
+import org.crsh.text.Style;
+import org.crsh.text.ui.LabelElement;
+import org.crsh.text.ui.RowElement;
+import org.crsh.text.ui.TableElement;
+
+import java.io.IOException;
+
+public class help extends BaseCommand {
+
+  @Usage("provides basic help")
+  @Command
+  public void main(InvocationContext<Object> context) throws IOException {
+
+    //
+    TableElement table = new TableElement().rightCellPadding(1);
+    table.add(
+        new RowElement().
+            add(new LabelElement("NAME").style(Style.style(Decoration.bold))).
+            add(new LabelElement("DESCRIPTION")));
+
+    //
+    CRaSH crash = (CRaSH)context.getSession().get("crash");
+    Iterable<String> names = crash.getCommandNames();
+    for (String name : names) {
+      try {
+        String desc = crash.getCommandDescription(name);
+        if (desc == null) {
+          desc = "";
+        }
+        table.add(
+            new RowElement().
+                add(new LabelElement(name).style(Style.style(Color.red))).
+                add(new LabelElement(desc)));
+      } catch (Exception ignore) {
+        //
+      }
+    }
+
+    //
+    context.provide(new LabelElement("Try one of these commands with the -h or --help switch:\n"));
+    context.provide(table);
+  }
+}
---
+++ crash/src/main/resources/crash/commands/cloudius/man.groovy
@@ -0,0 +1,20 @@
+package crash.commands.cloudius
+
+import org.crsh.cli.Required
+import org.crsh.command.DescriptionFormat
+import org.crsh.cli.Usage
+import org.crsh.cli.Command
+import org.crsh.cli.Argument;
+
+class man {
+  @Usage("format and display the on-line manual pages")
+  @Command
+  Object main(@Usage("the command") @Argument @Required String command) {
+    def cmd = crash.getCommand(command);
+    if (cmd != null) {
+      return  cmd.describe(unmatched, DescriptionFormat.MAN);
+    } else {
+      return "Command $command not found";
+    }
+  }
+}
