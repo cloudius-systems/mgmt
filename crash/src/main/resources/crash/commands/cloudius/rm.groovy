ba3ebb532a8c278da5ca6b2fb2e97fdd79f5d892
Nehal J Wani <nehaljw.kkd1@gmail.com>
2014-06-16 04:59:19 +0530

    Shell: Add more options to command: rm
    Fixes #36
    This patch adds -r, -i, -v and -f to the command: rm
    
    Signed-off-by: Nehal J Wani <nehaljw.kkd1@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- crash/src/main/resources/crash/commands/cloudius/rm.groovy
+++ crash/src/main/resources/crash/commands/cloudius/rm.groovy
@@ -9,31 +9,73 @@ import org.crsh.cli.descriptor.ParameterDescriptor
 import org.crsh.cli.spi.Completer
 import org.crsh.cli.spi.Completion
 
-// TODO: Allow multiple files to be specified
-
 class rm implements Completer {
   @Usage("delete file or directory")
   @Command
-  void main(
-  @Required @Argument(completer = rm.class) String path
-  ) {
-    file = getResolvedPath(path)
+  void main(@Option(names=['R', 'r', 'recursive']) @Usage('remove directories and their contents recursively') Boolean isRecursive,
+            @Option(names=['f', 'force']) @Usage('ignore nonexistent files and arguments, never prompt') Boolean isForceful,
+            @Option(names=['v', 'verbose']) @Usage('explain what is being done') Boolean isVerbose,
+            @Option(names=['i', 'interactive']) @Usage('prompt before every removal') Boolean isInteractive,
+            @Required @Argument(completer = rm.class) List<String> paths) {
 
-    if (!file.exists()) {
-      throw new ScriptException("no such file or directory")
-    }
-    if (file.isDirectory()) {
-      if (!file.delete()) {
-        throw new ScriptException("cannot delete directory")
+    def deleteStuff
+
+    deleteStuff = {
+      Boolean isDir = it.isDirectory()
+      String reply = null
+      if (!it.exists()) {
+        if (!isForceful) {
+          err.println("rm: cannot remove '${it}': No such file or directory")
+        }
+        return
       }
-      out.println("directory deleted")
-    }
-    if (file.isFile()) {
-      if (!file.delete()) {
-        throw new ScriptException("cannot delete file")
+      if (isDir) {
+        // Don't delete directory if -r is not specified
+        if (!isRecursive) {
+          err.println("rm: omitting directory '${it}'")
+          return
+        }
+        if (!isForceful && isInteractive) {
+          reply = context.readLine("rm: descend into directory '${it}'? ", true)
+          if (!(reply.size() > 0 &&
+                (reply.charAt(0) == 'y' || reply.charAt(0) == 'Y'))) {
+            return
+          }
+        }
+        // Recursively delete each File object in this directory (-r is specified)
+        it.eachFile(deleteStuff)
       }
-      out.println("file deleted")
+
+      if (!isForceful && isInteractive) {
+        reply = context.readLine("rm: remove " +
+                                 (isDir ? "directory" : "") + " '${it}'? ", true)
+        if (!(reply.size() > 0
+            && (reply.charAt(0) == 'y' || reply.charAt(0) == 'Y'))) {
+          return
+        }
+        /* Don't delete directory if it's contents were not deleted
+         * Control reaches here when using interactive + recursive mode, user has
+         * left some files inside a directory undeleted */
+        if (isDir && isRecursive && it.list().length > 0) {
+          err.println("rm: cannot remove '${it}': Directory not empty")
+          return
+        }
+      }
+
+      if (it.delete()) {
+        // -f overrides everything except -v
+        if (isVerbose) {
+          out.println("removed " + (isDir ? "directory" : "") + " '${it}'")
+        }
+      } else if (!isForceful) {
+        err.println("rm: cannot remove '${it}'")
+      }
     }
+
+    paths.each { path ->
+      deleteStuff(getResolvedPath(path))
+    }
+
   }
 
   @Override
